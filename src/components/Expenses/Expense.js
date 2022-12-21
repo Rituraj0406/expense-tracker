f7c6378f096ffb3c273f5e4963d26c57713f1d39
Rituraj0406 <riturajpiku01@gmail.com>
2022-12-21 23:09:40 +0530

    added filter select component and passed data from child to parent

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,12 +1,24 @@
+import React,{ useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expense(props){
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear =>{
+        // console.log('Expense.js');
+        // console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    }
     return (
         <div className="bg-slate-900 p-6 rounded-3xl">
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+            <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
+            <div>
+                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+            </div>
         </div>
     );
 }
