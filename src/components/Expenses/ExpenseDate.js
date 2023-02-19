2d8822f007fc251fcc00b1ac4ed1ddb135017090
Rituraj0406 <riturajpiku01@gmail.com>
2023-02-19 21:39:23 +0530

    dynamic list concept added and updated the list

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,7 +1,7 @@
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const month = props.date && props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date && props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date && props.date.getFullYear().toLocaleString();
 
   return (
     <div className="border-inherit border-2 rounded-3xl p-4 mr-10 bg-slate-900 text-white text-center w-28">
