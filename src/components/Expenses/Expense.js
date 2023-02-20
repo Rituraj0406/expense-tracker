0c47e5705713fb0321c680ed51224fe4947e49df
Rituraj0406 <riturajpiku01@gmail.com>
2023-02-20 21:52:26 +0530

    added filter method by year and the conditional statement also

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -8,18 +8,40 @@ function Expense(props){
     const filterChangeHandler = selectedYear =>{
         setFilteredYear(selectedYear);
     }
+
+    // filtered the list according to selected year
+    const filteredExpenses = props.items.filter(expenses => {
+        return expenses.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    let expensesContent = <p className='text-rose-500 font-bold text-3xl'>No Content Found.</p>;
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expenses) => (
+            <ExpenseItem
+                key={expenses.id} 
+                title={expenses.title}
+                amount={expenses.amount}
+                date={expenses.date}
+            />
+        ))
+    }
+
+
     return (
         <div className="bg-slate-900 p-6 rounded-3xl">
             <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
                 <div>
-                    {props.items.map((expenses) => (
+                    {expensesContent}
+                    {/* {filteredExpenses.length === 0 ? <p className='text-rose-500 font-bold text-3xl'>No expenses Found</p>: filteredExpenses.map((expenses) => (
                         <ExpenseItem
                             key={expenses.id} 
                             title={expenses.title}
                             amount={expenses.amount}
                             date={expenses.date}
                         />
-                    ))}
+                    ))} */}
                 </div>
         </div>
     );
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,8 @@
 function ExpenseDate(props) {
   const month = props.date && props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date && props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date && props.date.getFullYear().toLocaleString();
-
+  const year = props.date && props.date.getFullYear();
+  console.log('----date component',year);
   return (
     <div className="border-inherit border-2 rounded-3xl p-4 mr-10 bg-slate-900 text-white text-center w-28">
       <div className="font-semibold text-lg">{month}</div>
